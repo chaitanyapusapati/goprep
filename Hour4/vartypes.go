de2ec4fcd9bcdac9b039a15d9186bd8731db0291
CHAITANYA PUSAPATIter <chaitanyapusapati@gmail.com>
2019-12-12 23:11:10 +0530

    Variables Declaration, Assignment and Types

--- Hour4/vartypes.go
+++ Hour4/vartypes.go
@@ -2,15 +2,15 @@ package main
 
 import "fmt"
 
-var x, y int = 21,21
+var x, y int = 21, 21
 var z string
 
 func main() {
 	fmt.Println(x)
 	fmt.Println(1 + y)
-	z= "shaken!, Not Stirred"
+	z = "shaken!, Not Stirred"
 	fmt.Println(z)
-	a:=`This takes the raw string like :
+	a := `This takes the raw string like :
 			"Shaken, Not Stirred"
 						cause we are using back quotes`
 	fmt.Println(a)
