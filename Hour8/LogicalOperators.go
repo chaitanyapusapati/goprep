0de772bc93b5c9422658a0edabc370f645195ea1
CHAITANYA PUSAPATIter <chaitanyapusapati@gmail.com>
2019-12-23 16:43:55 +0530

    Conditional Logical Operators

---
+++ Hour8/LogicalOperators.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("true && true \t is \t", true && true)
+	fmt.Println("true && false \t is \t", true && false)
+	fmt.Println("true || true \t is \t", true || true)
+	fmt.Println("true || false \t is \t", true || false)
+	fmt.Println("!true \t \t is \t", !true)
+}
