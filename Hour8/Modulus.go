0a40b3f3ca6655b3313d548b6d7ea9dcbc1a6c1a
CHAITANYA PUSAPATIter <chaitanyapusapati@gmail.com>
2019-12-23 13:57:39 +0530

    Division and Modulus Operators

---
+++ Hour8/Modulus.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 83 / 40
+	y := 83 % 40
+	fmt.Println("Number 40 Divides 83 by", x, "times")
+	fmt.Println("Number 83 when Divided by 40 leaves Remainder", y)
+}
